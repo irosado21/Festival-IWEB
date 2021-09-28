e3f16fb8de84de9903857fcccba6090a102e2837
irosado21 <i_rosado@fpzornotza.com>
2021-09-28 12:38:20 +0200

    cambios completos del navbar2

--- js/inicio.js
+++ js/inicio.js
@@ -4,6 +4,8 @@ function pop() {
         document.getElementById("navbar2").style.display="none";
     }else{
         document.getElementById("navbar2").style.display="block";
+        document.getElementById("navbar").style.borderRadius="0";
+
     }   
 }
 
