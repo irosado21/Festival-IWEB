bfc80aa310c6344652d3d0f3478a2ae979b23c14
MarceloQuiroga <74302534+MarceloQuiroga@users.noreply.github.com>
2021-09-29 09:50:22 +0200

    tarjetas competas, faltan posicionar la hora

--- js/infoGroups.js
+++ js/infoGroups.js
@@ -1,17 +1,33 @@
 /*DATOS*/
 
-var Groups = ["Twenty_One_Pilots", "Metallica"]
+var Groups = [["Twenty_One_Pilots", "10:00"],
+              ["Metallica", "10:00"]]
+
+var lorem = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias a ipsum, temporibus eligendi ab nostrum architecto cum totam libero deleniti labore nesciunt modi voluptatum sed ipsam dignissimos praesentium quo tempore."
 
 /* FIN DATOS */
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    
-    Groups.forEach(element => {
-        console.log(document.getElementById("content").closest("div").closest("img"))
-       // document.getElementById("Content").closest.img.src = "/img/Grupos/" + element + ".png"
+
+    Groups.forEach(function (element,i) {
+        
+        document.getElementById("content").innerHTML += "<div class='content_x'>"
+                                                     +      "<div class='group'>"
+                                                     +          "<img src='/img/Grupos/" + element[0] + ".png' alt='Imagen del Grupo'>"
+                                                     +          "<p>" + lorem + "</p>"
+                                                     +      "</div>"
+                                                     +      "<div class='timeGroup'>"
+                                                     +          element[1]
+                                                     +      "</div>"
+                                                     +  "</div>"
+
+       
+        // document.getElementById("Content").closest.img.src = "/img/Grupos/" + element + ".png"
     });
-    
+
 })
+
+/*
 
 function iniGen() {
     imgGroups.forEach(element => {
@@ -27,6 +43,6 @@ function showInfo(group){
         }
     });
 }
-
+*/
 
 
