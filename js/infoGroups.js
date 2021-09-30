eb6f588b95061bb126c498b8fee7e578d2dc94f7
Azerzax <74302641+Azerzax@users.noreply.github.com>
2021-09-30 10:44:23 +0200

    Final

--- js/infoGroups.js
+++ js/infoGroups.js
@@ -1,7 +1,7 @@
 /*DATOS*/
 
-var Groups = [["Twenty_One_Pilots", "10:00"],
-              ["Imagine-Dragons", "10:00"]]
+var Groups = [["Twenty_One_Pilots", "10:00", "163"],
+              ["Imagine-Dragons", "16:00", "35"]]
 
 var lorem = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias a ipsum, temporibus eligendi ab nostrum architecto cum totam libero deleniti labore nesciunt modi voluptatum sed ipsam dignissimos praesentium quo tempore."
 
@@ -16,7 +16,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         document.getElementById("content").innerHTML += "<div class='content_x' data-group='" + element[0] + "'>"
                                                      +      "<div class='group'>"
                                                      +          "<img src='/img/Grupos/" + element[0] + ".png' alt='Imagen del Grupo'>"
-                                                     +          "<button onclick='showInfo(`" + element[0] + "`)'>Show Info</button>"
+                                                     +          "<button class='myBtn' onclick='showInfo(`" + Groups.indexOf(element) + "`)'>ENTRADAS</button>"
                                                      +          "<p>" + lorem + "</p>"
                                                      +      "</div>"
                                                      +      "<div class='timeGroup'>"
@@ -50,3 +50,28 @@ function showInfo(group){
   });
     
 }
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+function showInfo(x){
+  document.getElementById("myModal").style.display = "block";
+  document.getElementById("titlemodal").innerHTML=Groups[x][0];
+  document.getElementById("horariomodal").innerHTML="El concierto comenzara a las "+Groups[x][1];
+  document.getElementById("entradasmodal").innerHTML="Entradas disponiles: "+Groups[x][2];
+}
+
+// When the user clicks on <span> (x), close the modal
+function cerrar() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+
